1410580271d4a442ffccaeca418dba56e2cd3cbb
IINamelessII <justnitro26@gmail.com>
2019-03-18 03:27:22 +0200

    Add interpolation calculation

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from main import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('x_was_generated', views.generate_x, name='gen-x'),
+    path('y_by_interpolation', views.calc_y_by_interpolation, name='y-by-interpolation'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,29 @@
+from functools import reduce
 from math import cos
+from operator import mul
 from django.shortcuts import render
+
+
+def gen_xy():
+    x = [i * 0.3 for i in range(11)]
+    y = [3 * cos(i) ** 2 - i ** 0.5 for i in x]
+    return x, y
+
+
+def f(xs, ys):
+    if len(xs) == 1:
+        return ys[0]
+    else:
+        return (f(xs[1:], ys[1:]) - f(xs[:-1], ys[:-1])) / (xs[-1] - xs[0])
+
+
+def interpolation(x):
+    x_array, y_array = gen_xy()
+    return sum(
+        [f(x_array[:i + 1], y_array[:i + 1]) * 
+        reduce(mul, [x - x_array[j] for j in range(i)], 1) 
+        for i in range(len(x_array))]
+    )
 
 
 def index(request):
@@ -7,6 +31,10 @@ def index(request):
 
 
 def generate_x(request):
-    x = [i * 0.3 for i in range(11)]
-    y = [3 * cos(i) ** 2 - i ** 0.5 for i in x]
+    x, y = gen_xy()
     return render(request, 'index.html', {'x': x, 'y': y})
+
+def calc_y_by_interpolation(request):
+    x, y = gen_xy()
+    test = [interpolation(i) for i in x]
+    return render(request, 'index.html', {'x': x, 'y': y, 'test': test})
